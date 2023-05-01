91728e8b59b643229b94ba7f2cdc051486b37d8a
a007zope <a007zope@gmail.com>
2023-05-01 06:48:40 +0530

    added some new files

---
+++ src/Pages/RegPage.java
@@ -0,0 +1,9 @@
+package Pages;
+
+public class RegPage {
+	
+	int i =90;
+	
+	
+
+}
---
+++ src/tests/MyTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class MyTest {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("My Test");
+	}
+
+}
